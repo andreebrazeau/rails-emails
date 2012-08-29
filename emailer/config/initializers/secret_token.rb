15c14df3aa22180638fdb5d933be394d94d5ad78
Andree <andree@andree-laptop.(none)>
2012-08-28 17:12:35 -0700

    app created

---
+++ emailer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailer::Application.config.secret_token = '32344ab5e8505aba23d8b77a464b4154547c5134abae37464ebfb28774bd3c3a94f8730a826fd32e0fffe41d712cf16478226afb29d3df4c53c273728fcfa959'
